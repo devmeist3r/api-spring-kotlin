6bcafd46015ead51370ed533cb4bb0f1c399b0d0
devmeist3r <devmeist3r@gmail.com>
2021-02-25 20:23:39 -0300

    *

--- src/main/kotlin/com/devmeist3r/tour/controller/PromocaoController.kt
+++ src/main/kotlin/com/devmeist3r/tour/controller/PromocaoController.kt
@@ -1,9 +1,14 @@
 package com.devmeist3r.tour.controller
 
 import com.devmeist3r.tour.model.Promocao
+import com.devmeist3r.tour.model.RespostaJSON
 import com.devmeist3r.tour.service.PromocaoService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import java.util.*
+import javax.xml.ws.Response
 
 @RestController
 @RequestMapping(value = ["/promocoes"])
@@ -16,38 +21,69 @@ class PromocaoController {
    * Funcão para pegar promocao por id
    **/
   @GetMapping("/{id}")
-  fun getById(@PathVariable id: Long) = this.promocaoService.getById(id)
+  fun getById(@PathVariable id: Long): ResponseEntity<Promocao?> {
+    val promocao = this.promocaoService.getById(id)
+    val status = if (promocao == null) HttpStatus.NOT_FOUND else HttpStatus.OK
+    return ResponseEntity(promocao, status)
+  }
 
   /**
    * Funcão para criar uma nova promocao
    **/
   @PostMapping()
-  fun create(@RequestBody promocao: Promocao) {
-    this.create(promocao)
+  fun create(@RequestBody promocao: Promocao): ResponseEntity<RespostaJSON> {
+    this.promocaoService.create(promocao)
+    val respostaJSON = RespostaJSON("OK", Date())
+    return ResponseEntity(respostaJSON, HttpStatus.CREATED)
   }
 
   /**
    * Funcão para excluir uma promocao
    **/
   @DeleteMapping("/{id}")
-  fun delete(@PathVariable id: Long) {
-    this.delete(id)
+  fun delete(@PathVariable id: Long): ResponseEntity<Unit> {
+    val promocao = this.promocaoService.getById(id)
+    var status = HttpStatus.NOT_FOUND
+    if (promocao != null) {
+      status = HttpStatus.ACCEPTED
+      this.promocaoService.delete(id)
+    }
+    return ResponseEntity(Unit, status)
   }
 
   /**
    * Funcão para alteracao uma promocao
    **/
   @PutMapping("/{id}")
-  fun update(@PathVariable id: Long, @RequestBody promocao: Promocao) {
-    this.promocaoService.update(id, promocao)
+  fun update(@PathVariable id: Long, @RequestBody promocao: Promocao):
+      ResponseEntity<Unit> {
+    var status = HttpStatus.NOT_FOUND
+    if (this.promocaoService.getById(id) != null) {
+      this.promocaoService.update(id, promocao)
+      status = HttpStatus.ACCEPTED
+    }
+
+    return ResponseEntity(Unit, status)
   }
 
   /**
    * Funcão para listar todas as promocoes
    **/
   @GetMapping()
-  fun getAll(@RequestParam(required = false, defaultValue = "") localFilter:
-             String) = this.promocaoService.searchByLocal(localFilter)
+  fun getAll(
+    @RequestParam(required = false, defaultValue = "") localFilter:
+    String
+  ): ResponseEntity<List<Promocao?>> {
+    var status = HttpStatus.OK
+    val listaPromocoes = this.promocaoService.searchByLocal(localFilter)
+
+    if (this.promocaoService.searchByLocal(localFilter).isEmpty()) {
+      status = HttpStatus.NOT_FOUND
+    }
+
+    return ResponseEntity(listaPromocoes, status)
+
+  }
 
 
 }
---
+++ src/main/kotlin/com/devmeist3r/tour/model/RespostaJSON.kt
@@ -0,0 +1,5 @@
+package com.devmeist3r.tour.model
+
+import java.util.*
+
+data class RespostaJSON(val message: String, var dataHora: Date)
